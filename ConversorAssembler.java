e8059bf9d4bd5b8e203244a17088789a6e414da3
RobertoPintos <30840523+RobertoPintos@users.noreply.github.com>
2020-11-12 20:16:46 -0300

    Add files via upload

---
+++ ConversorAssembler.java
@@ -0,0 +1,130 @@
+package GeneracionDeCodigo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.*;
+
+import Lexico.Error;
+import Lexico.Controller;
+
+import Lexico.*;
+
+public class ConversorAssembler {
+
+	public static final String labelOverflowSuma = "LabelOverflowSuma";
+	public static final String labelDivCero = "LabelDivCero";
+	public static final String labelOverflowMul = "LabelOverflowMul";
+	public static final String labelEnd = "LabelEnd";
+
+	static File arch;
+	static TercetosController controladorTercetos;
+	static Controller controller;
+	private ArrayList<Error> erroresCI;
+	
+	public ConversorAssembler( TercetosController controladorTercetos, Controller controlador ) {
+		this.controladorTercetos = controladorTercetos;
+		this.controller = controlador;
+		erroresCI = new ArrayList<Error>();
+	}
+	
+	
+	public void generarAssembler (File r) throws IOException{
+		//controladorTercetos.generarAssembler();
+		String nameFile = r.getName().substring(0, r.getName().length()-4);
+		arch = new File(r.getParent() + "\\" + nameFile + ".asm");
+		writeFile();
+	}
+
+	private static void writeFile() throws IOException {
+		FileOutputStream fos = new FileOutputStream(arch);
+	 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		
+		
+		bw.write(".386" + '\n' 
+				+ ".model flat, stdcall" + '\n'
+				+ ".stack 200h" + '\n'
+				+ "option casemap :none" + '\n'
+				+ "include \\masm32\\include\\windows.inc" + '\n'
+				+ "include \\masm32\\include\\kernel32.inc" + '\n'
+				+ "include \\masm32\\include\\user32.inc" + '\n'
+				+ "include \\masm32\\include\\masm32.inc" + '\n'
+				+ "includelib \\masm32\\lib\\kernel32.lib" + '\n'
+				+ "includelib \\masm32\\lib\\user32.lib" + '\n'
+				+ "includelib \\masm32\\lib\\masm32.lib" + '\n'
+				+ '\n' +".data" + '\n');	
+		
+		
+		//Inicia el codigo
+		String code = "start:" + '\n' + (String) controladorTercetos.generarAssembler() + '\n'; 
+		
+		String data = "";
+		data = data + controller.generarAssemblerTS() ;
+		data = data + "DividirCero DB \"Error al dividir por cero!\", 0" + '\n';
+		data = data + "OverflowSuma DB \"Error por overflow en suma!\", 0" + '\n';
+		data = data + "OverflowMul DB \"Error por overflow en multiplicacion!\", 0" + '\n';
+		data = data + "_min_float_neg DD -3.40282347E38" + '\n';
+		data = data + "_max_float_pos DD 3.40282347E38" + '\n';
+		data = data + '\n' + ".code"+ "\n";
+		data = data + controladorTercetos.generarAssemblerFunctions() + "\n";
+		
+		
+		bw.write( data+"\n"+code );
+		String errores = labelDivCero + ":" + '\n';
+		errores = errores + "invoke MessageBox, NULL, addr DividirCero, addr DividirCero, MB_OK" + '\n';
+		errores = errores + "invoke ExitProcess, 0" + '\n';
+		errores = errores + labelOverflowSuma + ":" + '\n';
+		errores = errores + "invoke MessageBox, NULL, addr OverflowSuma, addr OverflowSuma, MB_OK" + '\n';
+		errores = errores + "invoke ExitProcess, 0" + '\n';
+		errores = errores + labelOverflowMul + ":" + '\n';
+		errores = errores + "invoke MessageBox, NULL, addr OverflowMul, addr OverflowMul, MB_OK" + '\n';
+		errores = errores + "invoke ExitProcess, 0" + '\n';
+		errores = errores + labelEnd + ":" + '\n';
+		errores = errores + "invoke ExitProcess, 0" + '\n';
+		bw.write(errores);
+		bw.write( "end start" );
+
+		bw.close();
+	}
+	
+	public void addErrorCI (String desc, int nLinea) {
+		Error e = new Error(desc, nLinea);
+		erroresCI.add(e);
+	}
+	
+	public ArrayList<Error> getErroresCI () {
+		return erroresCI;
+	}
+	
+	public boolean hayErroresCI () {
+		if (erroresCI.isEmpty())
+			return false;
+		else return true;
+	}
+	
+	public ConversorAssembler getConversorAssembler () {
+		return this;
+	}
+	
+	public void mostrarErrores(File f) {
+		try {
+			FileWriter fwriter = new FileWriter(f, true);
+			PrintWriter writer = new PrintWriter(fwriter);
+			writer.println("-----------------------------------");
+			writer.println("-----------------------------------");
+			writer.println("Lista de errores en Codigo Intermedio:");
+			for (Error e: erroresCI) {
+				writer.println("-"+e.getDescripcion()+" en la linea: "+e.getNroLinea());
+			}
+			writer.close();
+		} catch (Exception e) {
+    		e.printStackTrace();
+		}
+	}
+}
+
---
+++ prueba.java
@@ -0,0 +1,141 @@
+package Lexico;
+
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import java.io.FileReader;
+
+import Lexico.Controller;
+import Lexico.Fuente;
+import Sintactico.*;
+import GeneracionDeCodigo.*;
+
+public class prueba {
+	
+	private static BufferedReader codigo;
+	
+	//ESTE METODO GUARDA EL CODIGO EN UN STRINGBUILDER, AGREGANDO SALTOS DE LINEA Y EL SIMBOLO $ AL FINAL
+	private static StringBuilder getCodigo(BufferedReader ubicacion){
+        StringBuilder buffer = new StringBuilder();
+        try{
+            codigo = new BufferedReader(new FileReader(ubicacion.readLine()));
+            String readLine;
+            while ((readLine = codigo.readLine())!= null) {
+                buffer.append(readLine+"\n");
+            }
+            buffer.append("$");
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+	
+	 public static void mainr () {
+		
+		JFileChooser chooser = new JFileChooser();
+		String location = AppPrefs.FileLocation.get(System.getProperty("user.home"));
+	    chooser.setCurrentDirectory(new File(location));
+		int retVal = chooser.showOpenDialog(null);
+        if (retVal != chooser.APPROVE_OPTION)
+        	System.out.println("Archivo no encontrado.");
+        else {
+        	File selectedFile = chooser.getSelectedFile();
+        	AppPrefs.FileLocation.put(selectedFile.getParentFile().getAbsolutePath());
+        	String direccion = new String(selectedFile.getAbsolutePath());
+        	InputStream is = new ByteArrayInputStream(direccion.getBytes());
+        	BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+        	StringBuilder codigo = null;
+        	codigo = new StringBuilder(getCodigo(bf));
+        
+      //--------------------------------------------------------------------------------
+       
+        	Fuente archivo = new Fuente(codigo);
+        	Controller controlador = new Controller(archivo);
+            TercetosController tc = new TercetosController(controlador);
+            ConversorAssembler conversor = new ConversorAssembler(tc, controlador);
+
+        	//MUESTRO CODIGO POR CONSOLA, JUNTO CON SUS TOKENS DETECTADOS, TS, WARNINGS Y ERRORES.
+        	System.out.println("Codigo fuente:");
+        	System.out.println(codigo);
+        	System.out.println("--------------------------------");
+        	System.out.println("--------------------------------");
+        	
+        	Parser parser = new Parser(controlador, tc, conversor);
+            System.out.println(parser.yyparser());
+            
+
+        	
+            File f = new File (selectedFile.getParent()+"\\resultadoCompilacion.txt");
+        	try {
+					PrintWriter writer = new PrintWriter(f, "UTF-8");
+					writer.println("Codigo:");
+					writer.println(codigo);
+					writer.close();
+	        } catch (Exception e) {
+	        		e.printStackTrace();
+	        }
+      
+        	
+        	tc.mostrarTercetos(f);
+        	tc.printTercetos();
+        	
+        	
+        	if (controlador.hayErrores() || conversor.hayErroresCI()) {
+        		JOptionPane.showMessageDialog(null, "No se genera codigo assembler por errores en el codigo", null, JOptionPane.ERROR_MESSAGE);
+        	}else {
+        		try {
+        			/*System.out.println("----");
+                	System.out.println("----");
+                	System.out.println("Assembler:");
+                	System.out.println(".data");
+                	//System.out.println(controlador.generarAssemblerTS());
+                	System.out.println(".code");        	
+                	//System.out.println(tc.generarAssembler());*/
+        			conversor.generarAssembler(selectedFile);
+        			tc.mostrarTercetos(f);
+        			/*System.out.println("reduccion simple");
+        			tc.printTercetos();
+        			System.out.println("----");
+                	System.out.println("----");
+                	System.out.println("Assembler:");
+        			//System.out.println(controlador.generarAssemblerTS());*/
+        		} catch (IOException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		}
+          	}
+        	controlador.mostrarTablaSimbolos(f);
+        	controlador.mostrarListaTokens(f);
+        	controlador.getEstructuras(f);
+        	controlador.mostrarWarnings(f);
+        	controlador.mostrarErrores(f);
+        	conversor.mostrarErrores(f);
+        	tc.printTercetos();
+        }	
+	 }
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		mainr();
+	}
+
+}
+
